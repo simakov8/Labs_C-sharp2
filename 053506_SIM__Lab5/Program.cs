a300993a9fc73fe78c6cf4cdc600060e03fcedd9
EgorS <egor.sima.2003@gmail.com>
2021-09-25 00:13:42 +0300

    add lambda-handler

--- 053506_SIM__Lab5/Program.cs
+++ 053506_SIM__Lab5/Program.cs
@@ -12,15 +12,22 @@ namespace _053506_SIM__Lab1
       shop.ProductAdded += jornal.NewProductHandler;
       shop.CustomerAdded += jornal.NewCustomerHandler;
 
-      shop.AddProduct(new Product(10, 5, "gun"));
+      shop.PurchaseMaked += (Person person, Product product) =>
+      {
+        Console.WriteLine(person.FirstName + " bought " + product.Name);
+      };
+
+      shop.AddProduct(new Product(100, 5, "gun"));
       shop.AddProduct(new Product(36, 8, "pizza"));
       shop.AddProduct(new Product(94, 8, "car"));
       shop.AddProduct(new Product(65, 8, "house"));
       Person vasya = new Person("Vasya", "Pupkin");
+      Person petya = new Person("Petr", "Gubkin");
       shop.AddCustomer(vasya);
       vasya.ShowInfo();
       shop.MakePurchase(vasya, "car");
       shop.MakePurchase(vasya, "gun");
+      shop.MakePurchase(petya, "house");
       try
       {
         shop.MakePurchase(vasya, "galaxy");
