8f0e6c9c20dae90d81857c35b6e276ceb4d7fb8e
EgorS <egor.sima.2003@gmail.com>
2021-09-25 15:27:48 +0300

    usage example

--- 053506_SIM_Lab8/Program.cs
+++ 053506_SIM_Lab8/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _053506_SIM_Lab8
 {
@@ -10,6 +9,32 @@ namespace _053506_SIM_Lab8
   {
     static void Main(string[] args)
     {
+      List<Employee> employees1 = new List<Employee>();
+      employees1.Add(new Employee("Ivan", 34, true));
+      employees1.Add(new Employee("Petr", 46, false));
+      employees1.Add(new Employee("Gena", 28, false));
+      employees1.Add(new Employee("Anatoli", 30, true));
+      employees1.Add(new Employee("Yuri", 19, false));
+
+      string path1 = @".\employees1.dat";
+      string path2 = @".\employees2.dat";
+      using (File.Create(path1)) { };
+
+      FileService fileService = new FileService();
+      fileService.SaveData(employees1, path1);
+
+      File.Move(path1, path2);
+
+      List<Employee> employees2 = new List<Employee>(fileService.ReadFile(path2));
+      var sorted = employees2.OrderBy(empl => empl, new EmployeeComparer());
+
+      foreach (Employee employee in employees1)
+        employee.ShowInfo();
+
+      Console.WriteLine();
+
+      foreach (Employee employee in sorted)
+        employee.ShowInfo();
     }
   }
 }
