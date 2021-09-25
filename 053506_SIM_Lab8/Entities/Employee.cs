bed760f72e3ba78b99d922be0aece37dbb1158b3
EgorS <egor.sima.2003@gmail.com>
2021-09-25 15:07:03 +0300

    employee set

---
+++ 053506_SIM_Lab8/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _053506_SIM_Lab8
+{
+  class Employee
+  {
+    public Employee(string name, int age, bool isMarried)
+    {
+      Name = name;
+      Age = age;
+      IsMarried = isMarried;
+    }
+
+    public int Age { get; set; }
+    public bool IsMarried { get; set; }
+    public string Name { get; private set; }
+
+    public void ShowInfo()
+    {
+      Console.WriteLine("Name: {0}, Age: {1}, Is Married: {2}", Name, Age, IsMarried);
+    }
+  }
+}
---
+++ 053506_SIM_Lab8/Entities/EmployeeComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace _053506_SIM_Lab8
+{
+  class EmployeeComparer : IComparer<Employee>
+  {
+    public int Compare(Employee x, Employee y)
+    {
+      return x.Name.CompareTo(y.Name);
+    }
+  }
+}
