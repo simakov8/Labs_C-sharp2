8f7d923610b9e75b6f97211008a47716dd17c602
EgorS <egor.sima.2003@gmail.com>
2021-09-25 09:42:14 +0300

    new project

---
+++ 053506_SIM_Lab8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _053506_SIM_Lab8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
