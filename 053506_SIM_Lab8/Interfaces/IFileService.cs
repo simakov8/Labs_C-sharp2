9a93f73c9ac8768d4d7e30b88eb21ba076a3b8d5
EgorS <egor.sima.2003@gmail.com>
2021-09-25 15:26:08 +0300

    FileService set

---
+++ 053506_SIM_Lab8/Interfaces/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace _053506_SIM_Lab8
+{
+  interface IFileService
+  {
+    IEnumerable<Employee> ReadFile(string fileName);
+    void SaveData(IEnumerable<Employee> data, string fileName);
+  }
+}
---
+++ 053506_SIM_Lab8/Tools/FileService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace _053506_SIM_Lab8
+{
+  class FileService : IFileService
+  {
+    public IEnumerable<Employee> ReadFile(string fileName)
+    {
+      if (!File.Exists(fileName))
+        throw new FileNotFoundException();
+
+      using (BinaryReader reader = new BinaryReader(File.OpenRead(fileName)))
+      {
+        while (reader.PeekChar() > -1)
+        {
+          string name = reader.ReadString();
+          int age = reader.ReadInt32();
+          bool isMarried = reader.ReadBoolean();
+
+          yield return new Employee(name, age, isMarried);
+        }
+      }
+    }
+
+    public void SaveData(IEnumerable<Employee> data, string fileName)
+    {
+      if (!File.Exists(fileName))
+        throw new FileNotFoundException();
+
+      using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(fileName)))
+      {
+        foreach (var employee in data)
+        {
+          writer.Write(employee.Name);
+          writer.Write(employee.Age);
+          writer.Write(employee.IsMarried);
+        }
+      }
+    }
+  }
+}
